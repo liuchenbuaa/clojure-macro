b593679a00a31c41cad445d822fd68a16f4b1f90
liuchen <liuchen@zrcaifu.com>
2016-03-02 16:05:45 +0800

    test sth

--- src/clojure_macro/core.clj
+++ src/clojure_macro/core.clj
@@ -12,7 +12,52 @@
    `(let [or# ~x]
       (if or# or# (myor ~@next)))))
 
-(defmacro print
+(defmacro print124
   [t]
-  (list 'println t)
-  t)
+  t
+  )
+
+(defmacro infix
+  [params]
+  (list (second params) (first params) (last params)))
+
+(defmacro whenx
+  [cond & body]
+  (list 'if cond (cons 'do body)))
+
+(defmacro myprint [x]
+  (list (quote let) [(quote var) x]
+        (list 'println 'var)
+        'var))
+
+(defmacro whenxx
+  [cond & body]
+  (list 'if cond `(~cons do body)))
+
+(defmacro code-critic
+  [bad good]
+  (list 'do
+        (list 'println
+              "Great squid of Madrid, this is bad code:"
+              (list 'quote bad))
+        (list 'println
+              "Sweet gorilla of Manila, this is good code:"
+              (list 'quote good))))
+
+(defmacro critic
+  [bad good]
+  (list 'do
+        (list 'println
+              "this is the bad code"
+              (list 'quote bad))
+        (list 'println
+              "this is the good code"
+              (list 'quote good)
+         )
+   ))
+
+(defmacro critic2
+  [bad good]
+  `(do
+     (println "this is the bad code" (quote ~bad))
+     (println "this is the good code" (quote ~good))))
